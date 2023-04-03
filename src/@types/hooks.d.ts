bf77a39dd5802aa7d7152c29913aa5903aabf1fb
Lucas <lucas.garrafielo@academico.ufpb.br>
2023-04-03 19:13:57 -0300

    Footer e Header + Alterações pequenas no layout 🚀

---
+++ src/@types/hooks.d.ts
@@ -0,0 +1,17 @@
+import "@hyoretsu/react-hooks";
+import { Member as DefaultMember } from "@prisma/client";
+
+type Member = DefaultMember & {
+  type?: "decano";
+};
+
+type Tutor = Member & {
+  type: "tutor" | "founder";
+};
+
+declare module "@hyoretsu/react-hooks" {
+  interface APIMapping {
+    "/team/members": Member | Member[] | string;
+    "/team/tutors": Tutor[];
+  }
+}
