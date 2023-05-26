faecf481a2bb03a2d0bba405bae1390d111c8722
Aran Leite <hyoretsu@gmail.com>
2023-05-26 12:02:30 -0300

    Backend: exposição da tipagem atual da SDC para o frontend

--- frontend/src/@types/hooks.d.ts
+++ frontend/src/@types/hooks.d.ts
@@ -1,9 +1,20 @@
 import "@hyoretsu/react-hooks";
-import { Member as DefaultMember } from "backend";
+import {
+  Member as DefaultMember,
+  Project,
+  ProjectAttendance,
+  ProjectCertificate,
+  ProjectEdition,
+  ProjectEvent,
+  ProjectParticipant,
+  ProjectParticipation,
+  ProjectSpeaker,
+} from "backend";
 
 type APIError = {
   statusCode: number;
-  message: string;
+  message: string | string[];
+  error: string;
 };
 
 type Member = DefaultMember & {
@@ -16,7 +27,17 @@ type Tutor = DefaultMember & {
 
 declare module "@hyoretsu/react-hooks" {
   interface APIMapping {
+    "/projects": Project | APIError;
+    "/projects/certificates": ProjectCertificate[] | APIError;
+    "/projects/certificates/create": ProjectCertificate | APIError;
+    "/projects/certificates/validate": boolean | APIError;
+    "/projects/editions": ProjectEdition | ProjectEdition[] | APIError;
+    "/projects/editions/latest": ProjectEdition | APIError;
+    "/projects/events": ProjectEvent | APIError;
+    "/projects/participants": ProjectParticipant | APIError;
+    "/projects/participations": ProjectParticipation | APIError;
+    "/projects/speakers": ProjectSpeaker | APIError;
     "/team/members": Member | Member[] | APIError;
-    "/team/tutors": Tutor[];
+    "/team/tutors": Tutor[] | APIError;
   }
 }
