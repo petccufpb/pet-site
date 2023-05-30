e09c82c76523986c9936b75d15d56e744a85a45f
Lucas <lucas.garrafielo@academico.ufpb.br>
2023-05-30 20:26:36 -0300

    mais um ajuste

--- frontend/src/app/sdc/layout.tsx
+++ frontend/src/app/sdc/layout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from "react";
 import { Background } from "./components/Background";
 
 export const metadata: Metadata = {
-  title: "Semana da Computação - PET Computação UFPB",
+  title: "Plataforma | SDC - PET Computação UFPB",
   description:
     "Todo semestre o PET Computação (Programa de Educação Tutorial) realiza a Semana da Computação, queacontece junto com a entrada de novos estudantes na universidade. A Semana é dedicada para todosos profissionais da área de TI e ainda dá as boas-vindas aos novos estudantes dos cursos. Aprogramação contempla palestras da área de computação, empreendedorismo, minicursos, workshops,jogos, mesas redondas e mais.",
 };
@@ -13,14 +13,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <Head>
-        <title key={0}>Plataforma | SDC - PET Computação UFPB</title>
-        <meta
-          key={1}
-          name="description"
-          content="Todo semestre o PET Computação (Programa de Educação Tutorial) realiza a Semana da Computação, queacontece junto com a entrada de novos estudantes na universidade. A Semana é dedicada para todosos profissionais da área de TI e ainda dá as boas-vindas aos novos estudantes dos cursos. Aprogramação contempla palestras da área de computação, empreendedorismo, minicursos, workshops,jogos, mesas redondas e mais."
-        />
-      </Head>
       {children}
       <Background />
     </>
