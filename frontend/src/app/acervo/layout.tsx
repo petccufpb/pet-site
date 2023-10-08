b814e1e7c082c5298427a10c737eac1ea6fe16ff
Aran Leite <hyoretsu@gmail.com>
2023-10-08 01:57:46 -0300

    Fix deploy

--- frontend/src/app/acervo/layout.tsx
+++ frontend/src/app/acervo/layout.tsx
@@ -1,7 +1,5 @@
 import { PropsWithChildren } from "react";
 
-import { VideosProvider } from "@context/videos";
-
 export default function AcervoLayout({ children }: PropsWithChildren) {
   return <div style={{ margin: "4vh 0" }}>{children}</div>;
 }
