1a4d04dbb442db05111d3e1f4dea130fdae3bc3f
Aran Leite <hyoretsu@gmail.com>
2023-12-26 12:37:13 -0300

    Frontend: deletar import múltiplo por engano

--- frontend/src/app/acervo/video/[videoId]/page.tsx
+++ frontend/src/app/acervo/video/[videoId]/page.tsx
@@ -15,7 +15,6 @@ import Title from "../../components/Title";
 import { Video } from "../../playlist/[playlistId]/page";
 import MemberPhoto from "./components/MemberPhoto";
 import { Comment, Comments, Content, Description, Makers, VideoComments, Videos } from "./styles";
-import MemberPhoto from "./components/MemberPhoto";
 
 const makers: Member[] = [
   {
