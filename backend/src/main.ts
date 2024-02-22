ca57ba99cc0eac961b48650f08da74765af799e0
Aran Leite <hyoretsu@gmail.com>
2024-02-22 20:25:12 -0300

    Fix API authentication block

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { HttpException, HttpStatus, ValidationPipe } from "@nestjs/common";
-import { RequestHandler } from "@nestjs/common/interfaces";
-import { MiddlewareBuilder, NestFactory } from "@nestjs/core";
+import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as expressBasicAuth from "express-basic-auth";
 import helmet from "helmet";
@@ -19,16 +18,26 @@ const bootstrap = async () => {
     }),
   );
 
+  const users = (JSON.parse(process.env.SWAGGER_USERS || "[]") as string[][]).reduce(
+    (obj, [user, password]) => ({ ...obj, [user]: password }),
+    {} as Record<string, string>,
+  );
+
   app.use(helmet());
   // @ts-ignore
   app.use((req, res, next) => {
-    const urlRegex = /(https?)|(www.)|(:\d{1,4})|(\/$)/g;
+    const urlRegex = /(https?:\/\/)|(www.)|(\/$)/g;
+
+    const [user, pass] = Buffer.from(req.headers.authorization.split("Basic ")[1], "base64")
+      .toString()
+      .split(":");
 
     // Allow simple GET requests and disallow requests not originated from the frontend or for Swagger's favicon
     if (
       req.method !== "GET" &&
       req.url !== "/favicon.ico" &&
-      req.headers.origin?.replace(urlRegex, "") !== process.env.WEB_URL?.replace(urlRegex, "")
+      req.headers.origin?.replace(urlRegex, "") !== process.env.WEB_URL?.replace(urlRegex, "") &&
+      !(users[user] && pass === users[user])
     ) {
       throw new HttpException(
         `You don't have permission to access this API. Host - ${req.headers.origin}`,
@@ -44,10 +53,7 @@ const bootstrap = async () => {
     "/docs*",
     expressBasicAuth({
       challenge: true,
-      users: (JSON.parse(process.env.SWAGGER_USERS || "[]") as string[][]).reduce(
-        (obj, [user, password]) => ({ ...obj, [user]: password }),
-        {},
-      ),
+      users,
     }),
   );
 
